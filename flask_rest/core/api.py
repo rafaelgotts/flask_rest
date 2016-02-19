727a37f202be0e8b8a8fa92ed50b284cf56854c6
Rafael Gottsfritz <rafaelgotts@gmail.com>
2016-02-19 00:46:37 -0200

    Serializing with flask_restful

--- flask_rest/core/api.py
+++ flask_rest/core/api.py
@@ -1,19 +1,15 @@
 # -*- coding: utf-8 -*
 from __future__ import unicode_literals
 
-from flask_restful import Resource, marshal_with
+from flask_restful import Resource
 
 class BaseApi(Resource):
-    resource_fields = {}
 
-    @marshal_with(resource_fields)
     def get(self):
         raise NotImplementedError
 
-    @marshal_with(resource_fields)
     def post(self):
         raise NotImplementedError
 
-    @marshal_with(resource_fields)
     def put(self):
         raise NotImplementedError
--- flask_rest/tasks.py
+++ flask_rest/tasks.py
@@ -1,20 +1,26 @@
 # -*- coding: utf-8 -*
 from __future__ import unicode_literals
 
-from flask_restful import reqparse
+from flask_restful import reqparse, marshal_with
 from flask_rest.core.api import BaseApi
 
+
+"""
+    The reqparse receive and parser the args sent to the api.
+    But, like the python argparse, it's a little limited.
+"""
 parser = reqparse.RequestParser()
-parser.add_argument('task')
+parser.add_argument('task', type=str)
 
 
 """
     Representation of the fields in the resource
+    this fields are serialized on **reponse**
 """
 from flask_restful import fields
 basic_fields = {
     'task': fields.String,
-    'uri': fields.Url('basic_api')
+    'task_id': fields.Integer
 }
 
 """
@@ -26,11 +32,27 @@ class BasicApi(BaseApi):
     # So creative han?
     resource_fields=basic_fields
 
+    @marshal_with(basic_fields)
     def post(self, todo_id):
-        args = parser.parse_args()
-        task = {'task': args['task']}
-        TODOS[todo_id] = task
-        return task, 201
+        """
+            Receive and serialize the fields.
+            If they cannot parse, then an error is returned
+        """
+        parsed_args = parser.parse_args()
+        task_description = parsed_args['task']
+
+        """ Simulates the database
+        """
+        TODOS[todo_id] = {'task': task_description}
+
+        """
+            Now, i will create the data for the response
+            and the marshal_with will serialize based on
+            dict basic_fields.
+        """
+        data = {'task_id': todo_id, 'task': task_description}
+
+        return data, 201
 
     def get(self, todo_id):
         return TODOS[todo_id]
